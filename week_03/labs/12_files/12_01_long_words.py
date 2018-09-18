d656d1f4256a42f5d7fbd46360ec4ff8caa37458
thetonyluce <thetonyluce@gmail.com>
2018-09-18 15:44:33 +0200

    small change

--- week_03/labs/12_files/12_01_long_words.py
+++ week_03/labs/12_files/12_01_long_words.py
@@ -7,7 +7,7 @@ Source: http://greenteapress.com/thinkpython2/html/thinkpython2010.html
 '''
 
 with open('12_files/words2.txt', 'r') as f: #open this file as variable f
-for word in f.readlines():                  #read file line by line
+    for word in f.readlines():                  #read file line by line
         word = word.rstrip()                #strip whitespace characters
         if len(word) > 20:                  #if length of word > 20
             print(word)                     #print results
